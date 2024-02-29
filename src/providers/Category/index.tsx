3f8d7fd431b10470f3e9f26d9ce003ceda16fbca
Eduardo Parraga <eduardo.parraga95@gmail.com>
2024-02-29 10:25:05 -0300

    async category related api provider functions

--- src/providers/Category/index.tsx
+++ src/providers/Category/index.tsx
@@ -30,29 +30,30 @@ export const CategoryProvider: React.FC<{ children: React.ReactNode }> = ({
   const navigate = useNavigate();
 
   // fetch category data
-  const getCategoryData = (category_id: number) => {
+  const getCategoryData = async (category_id: number) => {
     hasValidSession();
 
-    api
-      .get(`/categories/${category_id}`, {
+    try {
+      const res = await api.get(`/categories/${category_id}`, {
         headers: {
           Authorization: `Bearer ${accToken}`,
         },
-      })
-      .then((res) => {
-        setCategoryData(res.data);
-      })
-      .catch((err: any) => {
-        console.log(err);
       });
+      setCategoryData(res.data);
+    } catch (err: any) {
+      console.log(err);
+    }
   };
 
   // create category
-  const createCategory = (formData: ICategoryCreate, event_id: number) => {
+  const createCategory = async (
+    formData: ICategoryCreate,
+    event_id: number
+  ) => {
     hasAdminRights();
 
-    api
-      .post(
+    try {
+      const res = await api.post(
         "/categories",
         {
           ...formData,
@@ -63,126 +64,117 @@ export const CategoryProvider: React.FC<{ children: React.ReactNode }> = ({
             Authorization: `Bearer ${accToken}`,
           },
         }
-      )
-      .then((res) => {
-        console.log(res.data);
-        const { category_id } = res.data;
+      );
+      console.log(res.data);
+      const { category_id } = res.data;
 
-        if (res.status === 201) {
-          toast.success("Categoria criada com sucesso");
-          navigate(`/admin/events/${event_id}/categories/${category_id}`);
-        }
+      if (res.status === 201) {
+        toast.success("Categoria criada com sucesso");
         navigate(`/admin/events/${event_id}/categories/${category_id}`);
-      })
-      .catch((err: any) => {
-        console.log(err);
-        toast.error("Algo deu errado");
-        navigate(`/admin/events/${event_id}`);
-      });
+      }
+      navigate(`/admin/events/${event_id}/categories/${category_id}`);
+    } catch (err: any) {
+      console.log(err);
+      toast.error("Algo deu errado");
+      navigate(`/admin/events/${event_id}`);
+    }
   };
 
   // player join category
-  const joinCategory = (event_id: number, category_id: number) => {
+  const joinCategory = async (event_id: number, category_id: number) => {
     hasValidSession();
 
-    api
-      .patch(`/categories/${category_id}/join`, null, {
+    try {
+      const res = await api.patch(`/categories/${category_id}/join`, null, {
         headers: {
           Authorization: `Bearer ${accToken}`,
         },
-      })
-      .then((res) => {
-        console.log(res.data.response);
-        if (res.status === 200) {
-          toast.success("Adicionado na categoria com sucesso!");
-
-          if (decodedPlayerInfo.role === "admin") {
-            navigate(`/admin/events/${event_id}/categories/${category_id}`);
-          } else {
-            navigate(`/dashboard/events/${event_id}/categories/${category_id}`);
-          }
-        }
-      })
-      .catch((err: any) => {
-        console.log(err);
-        if (
-          err.response.data.message ===
-          "Player already assigned to this Category"
-        ) {
-          toast.error("Jogador já cadastrado na categoria");
-          if (decodedPlayerInfo.role === "admin") {
-            navigate(`/admin/events/${event_id}/categories/${category_id}`);
-          } else {
-            navigate(`/dashboard/events/${event_id}/categories/${category_id}`);
-          }
-        } else {
-          toast.error("Algo deu errado");
-          if (decodedPlayerInfo.role === "admin") {
-            navigate(`/admin/events/${event_id}/categories/${category_id}`);
-          } else {
-            navigate(`/dashboard/events/${event_id}/categories/${category_id}`);
-          }
-        }
       });
+      console.log(res.data.response);
+      if (res.status === 200) {
+        toast.success("Adicionado na categoria com sucesso!");
+
+        if (decodedPlayerInfo.role === "admin") {
+          navigate(`/admin/events/${event_id}/categories/${category_id}`);
+        } else {
+          navigate(`/dashboard/events/${event_id}/categories/${category_id}`);
+        }
+      }
+    } catch (err: any) {
+      console.log(err);
+      if (
+        err.response.data.message === "Player already assigned to this Category"
+      ) {
+        toast.error("Jogador já cadastrado na categoria");
+        if (decodedPlayerInfo.role === "admin") {
+          navigate(`/admin/events/${event_id}/categories/${category_id}`);
+        } else {
+          navigate(`/dashboard/events/${event_id}/categories/${category_id}`);
+        }
+      } else {
+        toast.error("Algo deu errado");
+        if (decodedPlayerInfo.role === "admin") {
+          navigate(`/admin/events/${event_id}/categories/${category_id}`);
+        } else {
+          navigate(`/dashboard/events/${event_id}/categories/${category_id}`);
+        }
+      }
+    }
   };
 
-  // remove player from category
-
   // update category
-  const updateCategory = (formData: ICategoryUpdate, category: ICategory) => {
+  const updateCategory = async (
+    formData: ICategoryUpdate,
+    category: ICategory
+  ) => {
     hasAdminRights();
 
     const { category_id, event: event_id } = category;
 
-    api
-      .patch(`/categories/${category_id}`, formData, {
+    try {
+      const res = await api.patch(`/categories/${category_id}`, formData, {
         headers: {
           Authorization: `Bearer ${accToken}`,
         },
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          toast.success("Informações da categoria atualizadas");
-          navigate(`/admin/events/${event_id}/categories/${category_id}`);
-        } else {
-          toast.error("Algo deu errado");
-          navigate(`/admin/events/${event_id}/categories/${category_id}`);
-        }
-        console.log(res);
-      })
-      .catch((err: any) => {
-        console.log(err);
-        if (err.response.data.message === "Internal server error") {
-          toast.error("Algo deu errado");
-          navigate(`/admin/events/${event_id}/categories/${category_id}`);
-        }
-        navigate(`/admin/events/${event_id}/categories/${category_id}`);
       });
+      if (res.status === 200) {
+        toast.success("Informações da categoria atualizadas");
+        navigate(`/admin/events/${event_id}/categories/${category_id}`);
+      } else {
+        toast.error("Algo deu errado");
+        navigate(`/admin/events/${event_id}/categories/${category_id}`);
+      }
+      console.log(res);
+    } catch (err: any) {
+      console.log(err);
+      if (err.response.data.message === "Internal server error") {
+        toast.error("Algo deu errado");
+        navigate(`/admin/events/${event_id}/categories/${category_id}`);
+      }
+      navigate(`/admin/events/${event_id}/categories/${category_id}`);
+    }
   };
 
   // delete category
-
-  const deleteCategory = (category_id: number, event_id: number) => {
+  const deleteCategory = async (category_id: number, event_id: number) => {
     hasAdminRights();
 
-    api
-      .delete(`/categories/${category_id}`, {
+    try {
+      const res = await api.delete(`/categories/${category_id}`, {
         headers: {
           Authorization: `Bearer ${accToken}`,
         },
-      })
-      .then((res) => {
-        console.log(res);
-        if (res.status === 200) {
-          toast.success("Categoria deletada com sucesso");
-          navigate(`/admin/events/${event_id}`);
-        }
-        navigate(`/admin/events/${event_id}`);
-      })
-      .catch((err: any) => {
-        console.log(err);
-        navigate(`/admin/events/${event_id}`);
       });
+      console.log(res);
+      if (res.status === 200) {
+        toast.success("Categoria deletada com sucesso");
+        navigate(`/admin/events/${event_id}`);
+      }
+      navigate(`/admin/events/${event_id}`);
+    } catch (err: any) {
+      console.log(err);
+      navigate(`/admin/events/${event_id}`);
+    }
   };
 
   return (
