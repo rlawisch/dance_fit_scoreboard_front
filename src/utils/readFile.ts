7c99c45f5af72c716b36e99a3caa80bb3cdde4f4
Eduardo Parraga <eduardo.parraga95@gmail.com>
2024-03-06 09:27:41 -0300

    fix: unused import

--- src/utils/readFile.ts
+++ src/utils/readFile.ts
@@ -1,5 +1,3 @@
-import { Readable } from "stream"
-
 export const readFile = (file: File): Promise<string | ArrayBuffer | null> => {
     return new Promise((resolve) => {
         const reader = new FileReader()
