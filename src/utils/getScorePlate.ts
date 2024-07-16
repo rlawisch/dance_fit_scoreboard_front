f6f2c91a0d5e5f47d0f263e8a2cb4000c06c97ed
Eduardo Parraga <eduardo.parraga95@gmail.com>
2024-07-16 20:17:00 -0300

    fix: getScorePlate function conditions order

--- src/utils/getScorePlate.ts
+++ src/utils/getScorePlate.ts
@@ -5,7 +5,6 @@ export const getScorePlate = (
   miss: number,
   stage_pass: boolean
 ) => {
-
   if (!!stage_pass) {
     if (miss >= 21) {
       return "RG";
@@ -15,14 +14,14 @@ export const getScorePlate = (
       return "TG";
     } else if (miss >= 1 && miss <= 5) {
       return "MG";
-    } else if (miss === 0) {
-      return "SG";
-    } else if (bad + miss === 0) {
-      return "EG";
-    } else if (good + bad + miss === 0) {
-      return "UG";
     } else if (great + good + bad + miss === 0) {
       return "PG";
+    } else if (good + bad + miss === 0) {
+      return "UG";
+    } else if (bad + miss === 0) {
+      return "EG";
+    } else if (miss === 0) {
+      return "SG";
     }
   }
 
