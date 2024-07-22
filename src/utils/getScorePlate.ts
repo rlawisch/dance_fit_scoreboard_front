e567cef1493b2cbd92e40da9b2095e7d320a0dc0
Eduardo Parraga <eduardo.parraga95@gmail.com>
2024-07-22 12:55:46 -0300

    refac: eliminating redundant else clauses and refining if conditions for getScorePlate

--- src/utils/getScorePlate.ts
+++ src/utils/getScorePlate.ts
@@ -5,22 +5,29 @@ export const getScorePlate = (
   miss: number,
   stage_pass: boolean
 ) => {
-  if (!!stage_pass) {
+  if (stage_pass) {
     if (miss >= 21) {
       return "RG";
-    } else if (miss >= 11 && miss <= 20) {
+    }
+    if (miss >= 11) {
       return "FG";
-    } else if (miss >= 6 && miss <= 10) {
+    }
+    if (miss >= 6) {
       return "TG";
-    } else if (miss >= 1 && miss <= 5) {
+    }
+    if (miss >= 1) {
       return "MG";
-    } else if (great + good + bad + miss === 0) {
+    }
+    if (great + good + bad + miss === 0) {
       return "PG";
-    } else if (good + bad + miss === 0) {
+    }
+    if (good + bad + miss === 0) {
       return "UG";
-    } else if (bad + miss === 0) {
+    }
+    if (bad + miss === 0) {
       return "EG";
-    } else if (miss === 0) {
+    }
+    if (miss === 0) {
       return "SG";
     }
   }
